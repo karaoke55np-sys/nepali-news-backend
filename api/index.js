e3b86ec9c5b697728694eda5715f5b05a33661dc
Sujan <sujan@gmail.com>
2026-03-02 21:27:53 +0545

    Fresh minimal backend

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/api/news", (req, res) => {
-  res.json({ message: "API working successfully 🚀" });
-});
-
-module.exports = app;
